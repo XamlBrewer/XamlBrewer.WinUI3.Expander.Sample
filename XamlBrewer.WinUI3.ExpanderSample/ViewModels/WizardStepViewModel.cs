64331e31e2f492561dde1048725f5b48a124665c
Diederik Krols <diederik_krols@hotmail.com>
2022-06-08 07:03:42 +0200

    Wizard ViewModels

--- XamlBrewer.WinUI3.ExpanderSample/ViewModels/WizardStepViewModel.cs
+++ XamlBrewer.WinUI3.ExpanderSample/ViewModels/WizardStepViewModel.cs
@@ -1,13 +1,37 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace XamlBrewer.WinUI3.ExpanderSample.ViewModels
 {
     internal partial class WizardStepViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private WizardViewModel _wizard;
+
+        [ObservableProperty]
+        private string _name;
+
+        [ObservableProperty]
+        private bool _allowReturn = true;
+
+        [ObservableProperty]
+        private bool _isFinal;
+
+        [ObservableProperty]
+        private bool _isFirst;
+
+        public WizardStepViewModel(WizardViewModel wizard)
+        {
+            _wizard = wizard;
+        }
+
+        public ICommand PreviousCommand { get; set; }
+
+        public ICommand NextCommand { get; set; }
+
+        public void Enter()
+        { }
+
+
     }
 }
--- XamlBrewer.WinUI3.ExpanderSample/ViewModels/WizardViewModel.cs
+++ XamlBrewer.WinUI3.ExpanderSample/ViewModels/WizardViewModel.cs
@@ -1,13 +1,36 @@
 ﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace XamlBrewer.WinUI3.ExpanderSample.ViewModels
 {
     internal partial class WizardViewModel : ObservableObject
     {
+        [ObservableProperty]
+        private List<WizardStepViewModel> _steps = new();
+
+        [ObservableProperty]
+        private WizardStepViewModel _current;
+
+        internal WizardStepViewModel PreviousStep(WizardStepViewModel step)
+        {
+            var stepIndex = _steps.IndexOf(step);
+            if (stepIndex < _steps.Count)
+            {
+                return _steps[stepIndex + 1];
+            }
+
+            return null;
+        }
+
+        internal WizardStepViewModel NextStep(WizardStepViewModel step)
+        {
+            var stepIndex = _steps.IndexOf(step);
+            if (stepIndex > 0)
+            {
+                return _steps[stepIndex - 1];
+            }
+
+            return null;
+        }
     }
 }
